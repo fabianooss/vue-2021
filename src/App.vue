5a0fc04eee8aed5fd937afe3ef694a7c3a4bc7c1
FABIANO OSS <fabiano.oss@prof.sc.senac.br>
2021-09-10 19:32:20 -0300

    adicionar e alterar

--- src/App.vue
+++ src/App.vue
@@ -7,16 +7,43 @@
     <div v-show="mostrarInformacoesComplementares">
         <h2>Informações adicionais</h2>
 
-        <ul>
-           <li v-for="p in projetos" :key="p.nome">{{p.nome}}</li>
-        </ul>
+        <form @submit.prevent="salvarProjeto">
+            <label for="nome">Nome:</label><br/>
+            <input type="text" id="nome" autofocus required v-model="projeto.nome"/> <br/>
+            <label for="tamanho">Tamanho:</label><br/>
+            <input type="number" id="tamanho" required min="0" max="100" v-model="projeto.tamanho"/> <br/>
+            
+            <div v-if="alteracaoIdx > -1">
+              <input type="submit" value="Alterar projeto" />
+            </div>
+            <div v-else>
+              <input type="submit" value="Adicionar projeto" />
+            </div>
+
+        </form>  
+
+
+        <table>
+          <tr>
+            <th>Nome</th>
+            <th>Tamanho</th>
+            <th></th>
+          </tr>
+          <tr v-for="(p, idx) in projetos" :key="p.nome">
+            <td> {{p.nome}} </td>
+            <td> {{p.tamanho}} </td>
+            <td> 
+              <button @click="alterar(idx)">Alterar</button>
+              <button @click="remover(p)">Remover</button> 
+            </td>
+          </tr>
+        </table>
 
     </div> 
   </div>
 </template>
 
 <script>
-
 export default {
   data() {
     return {   //JSON - Javascript Object Notation
@@ -28,13 +55,51 @@ export default {
         {nome: "HTML", tamanho: 100 },
         {nome: "CSS", tamanho: 80},
         {nome: "JavaScript", tamanho: 200}
-      ]
+      ],
+      projeto: {
+        nome: null,
+        tamanho: 0
+      },
+      alteracaoIdx: -1
     }
   },
   methods: {
     trocarInformacoes() {
       this.mostrarInformacoesComplementares = !this.mostrarInformacoesComplementares
-    } 
+    },
+    salvarProjeto() {
+/*      this.projetos.push(
+        {
+          nome: this.projeto.nome,
+          tamanho: this.projeto.tamanho  
+        }
+      )      */
+      const projetoSalvar = Object.assign({}, this.projeto)
+      if (this.alteracaoIdx > -1) {
+        this.projetos[this.alteracaoIdx] = projetoSalvar
+      }
+      else {
+        this.projetos.push(projetoSalvar)
+      }
+
+      this.reset()
+    },
+    reset() {
+      this.projeto.nome = null
+      this.projeto.tamanho = 0
+      this.alteracaoIdx = -1
+    },
+    remover(projetoParaRemover) {
+      if (confirm("Excluir registro?") ) {
+        // this.projetos.splice(this.projetos.indexOf(projetoParaRemover), 1)
+        this.projetos = this.projetos.filter(p => p != projetoParaRemover )
+        this.reset()
+      }
+    },
+    alterar(idx) {
+      this.projeto = Object.assign({}, this.projetos[idx]) 
+      this.alteracaoIdx = idx
+    }
   }
 }
 </script>
